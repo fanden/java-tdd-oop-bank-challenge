f63aee0dd74bee14d1ad3e16a0de11082af9c2c5
Alfred Ryvarden <alfredryvarden@gmail.com>
2025-08-13 21:04:08 +0200

    added current account tests

--- src/test/java/com/booleanuk/core/CurrentAccountTest.java
+++ src/test/java/com/booleanuk/core/CurrentAccountTest.java
@@ -1,4 +1,29 @@
 package com.booleanuk.core;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class CurrentAccountTest {
+
+    @Test
+    public void testOverdraftLimit() {
+        Branch testBranch = new Branch("oslo");
+        User testCustomer = new Customer("Trond Giske");
+        UUID testUserUUID = testCustomer.getUUID();
+
+        testBranch.createCurrentAccountForUser(testUserUUID);
+        CurrentAccount testAccount = (CurrentAccount) testBranch.getAccountsForCustomerID(testUserUUID).getFirst();
+
+        testAccount.setOverdraftLimit(222);
+        assertEquals(222, testAccount.getOverdraftLimit());
+
+        testAccount.setOverdraftApproved(true);
+        assertTrue(testAccount.isOverdraftApproved());
+
+        testAccount.setOverdraftLimit(333);
+        assertFalse(testAccount.isOverdraftApproved());
+    }
 }
